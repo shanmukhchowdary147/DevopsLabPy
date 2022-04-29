cecc45aefeb44454957f5e21661c19e7e3e0014c
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2022-04-29 11:06:11 +0530

    h

---
+++ ll.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+        System.out.println("Name: Devops, Credit: 3")
+        System.out.println("Name: Software Project Management, Credit: 3");
+        System.out.println("Name: Career Skills for IT Companies, Credit: 3");
+        System.out.println("Name: Seminar, Credit: 1");
+        System.out.println("Name: Marketing, Credit: 3");
+	}
+}
