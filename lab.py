63721d4d79f82504f7604d51a7ce4e200d718ca6
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2022-04-29 09:57:14 +0530

    first commit

---
+++ lab.py
@@ -0,0 +1,8 @@
+print("Name: Devops, Credit: 3")
+print("Name: Software Project Management, Credit: 3")
+print("Name: Career Skills for IT Companies, Credit: 3")
+print("Name: High Performance Computing, Credit: 4")
+print("Name: Capstone Project, Credit: 1")
+print("Name: Undergrad Research, Credit: 1")
+print("Name: Seminar, Credit: 1")
+print("Name: Marketing, Credit: 3")
